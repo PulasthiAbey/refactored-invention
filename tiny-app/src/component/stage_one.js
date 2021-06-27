03bd0239e7527f082aa3d768ed674b59ae197676
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-06-28 02:25:09 +0530

    Development stages

--- tiny-app/src/component/stage_one.js
+++ tiny-app/src/component/stage_one.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import { Input, Button, ListItem, Text } from "react-native-elements";
+
+import { MyContext } from "../context";
 
 const StageOne = () => {
+  const context = useContext(MyContext);
   return (
     <view>
       <Text>Stage One Opened</Text>
