a7dfefefb8fdddf77b7ada836a8f0e09cba3889f
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-06-28 01:44:58 +0530

    Development stages

--- tiny-app/App.js
+++ tiny-app/App.js
@@ -6,7 +6,6 @@ class App extends Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
       </View>
     );
   }
