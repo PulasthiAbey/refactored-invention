884f1b187bb62d818da2ec55c27662f2e38688e8
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-06-30 11:44:22 +0530

    Edited

--- tiny-app/src/component/stage_one.js
+++ tiny-app/src/component/stage_one.js
@@ -9,71 +9,70 @@ import { MyContext } from "../context";
 const StageOne = () => {
   const context = useContext(MyContext);
   return (
-    <Formik
-      initialValue={{ player: "" }}
-      validationSchema={Yup.object({
-        player: Yup.string()
-          .min(3, "Must be more than 3 characters")
-          .max(15, "Less than 15 characters are allowed")
-          .required("Sorry the name is required"),
-      })}
-      onSubmit={(values, { resetForm }) => {
-        context.addPlayer(values.player);
-        resetForm();
-      }}
-    >
-      {({
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        values,
-        touched,
-        errors,
-      }) => (
-        <>
-          <Text>Tinny App</Text>
-          {/* //input line of the interface */}
-          <Input
-            placeholder="Name"
-            leftIcon={{ type: "antdesign", name: "adduser" }}
-            inputContainerStyle={{
+    <>
+      <Formik
+        initialValue={{ player: "" }}
+        validationSchema={Yup.object({
+          player: Yup.string()
+            .min(3, "Must be more than 3 characters")
+            .max(15, "Less than 15 characters are allowed")
+            .required("Sorry the name is required"),
+        })}
+        onSubmit={(values, { resetForm }) => {
+          context.addPlayer(values.player);
+          resetForm();
+        }}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          touched,
+          errors,
+        }) => (
+          <>
+            <Text>Tinny App</Text>
+            {/* //input line of the interface */}
+            <Input
+              placeholder="Name"
+              leftIcon={{ type: "antdesign", name: "adduser" }}
+              inputContainerStyle={{
+                marginHorizontal: 50,
+                marginTop: 50,
+              }}
+              onChangeText={handleChange("player")}
+              onBlur={handleBlur("player")}
+              value={values.player}
+            />
+            {/* Error Messages */}
+            renderErrorMessage={errors.player && touched.player}
+            errorMessage={errors.player}
+            errorStyle=
+            {{
               marginHorizontal: 50,
-              marginTop: 50,
+              marginTop: 5,
             }}
-            onChangeText={handleChange("player")}
-            onBlur={handleBlur("player")}
-            value={values.player}
-          />
-          {/* Error Messages */}
-          renderErrorMessage={errors.player && touched.player}
-          errorMessage={errors.player}
-          errorStyle=
-          {{
-            marginHorizontal: 50,
-            marginTop: 5,
-          }}
-          {/* Button of the page */}
-          <Button
-            buttonStyle={styles.button}
-            title="Add Player"
-            onPress={handleSubmit}
-          />
-        </>
-      )}
-    </Formik>
-    // Viewing the intems in the player list
-    <View style={{padding:20, width:'100%'}}>
-      {
-        context.state.players && context.state.players.length >0 ?
-        <>
-          <Text>List of Players</Text>
-        </>
-
-        :null
-      }
-    </View>
-  )
-}
+            {/* Button of the page */}
+            <Button
+              buttonStyle={styles.button}
+              title="Add Player"
+              onPress={handleSubmit}
+            />
+          </>
+        )}
+      </Formik>
+      // Viewing the intems in the player list
+      <View style={{ padding: 20, width: "100%" }}>
+        {context.state.players && context.state.players.length > 0 ? (
+          <>
+            <Text>List of Players</Text>
+          </>
+        ) : null}
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   button: {
