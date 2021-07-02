4eec32bf817482c12478a4fe460ebfd0bd3319c2
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-02 10:15:34 +0530

    Tiny app edited

--- tiny-app/src/utils/tools.js
+++ tiny-app/src/utils/tools.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
 export const MainLogo = () => {
-
-    
-}
+  <Text
+    style={{
+      fontFamily: "Pacifico - Regular",
+      color: "#db3eb1",
+      textDecorationLine: "underline",
+      textDecorationColor: "#41b6e6",
+      fontSize: 30,
+    }}
+  ></Text>;
+};
